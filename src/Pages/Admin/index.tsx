df929f9ef5e63232ba98b4f6ce25653b891c8d24
MarianaDeleau <marianadeleau@gmail.com>
2022-01-09 10:33:24 -0300

    clean code

--- src/Pages/Admin/index.tsx
+++ src/Pages/Admin/index.tsx
@@ -1,11 +1,8 @@
 import { FC } from 'react'
 import { BasicPagination } from '../../components';
 import { CardMovie } from '../../components/parts/CardMovie';
-import { useItems } from '../../hooks/useItems';
 
 const Admin: FC = () => {
-
-  const { setPage, page, lastPage,  } = useItems();  
 
 
     return (
--- src/components/parts/Paginator/index.tsx
+++ src/components/parts/Paginator/index.tsx
@@ -3,58 +3,8 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import { FC } from 'react'
 import {  makeStyles } from '@mui/material/styles'
-import { setSourceMapRange } from 'typescript';
 import { useItems } from '../../../hooks/useItems';
 
-
-// type Props = {
-//     currentPage: number;
-//     totalPages: string;
-//     changePages: (newPage: number) => void;
-// }
-
-// const Paginator: FC<Props> = ({currentPage, totalPages, changePages}) => {
-    
-
-//     return (
-//         <div id="resultsPagination" className="results__pagination">
-//             <button className="paginationBtn" onClick={() => changePages(Number(totalPages)-(Number(totalPages) - 1))}>
-//                 <i className="fas fa-backward"></i>
-//             </button>
-//             <button className="paginationBtn" onClick={() => changePages(currentPage-1)}>
-//                 <i className="fas fa-step-backward"></i>
-//             </button>
-//             <button className="paginationBtn" onClick={() => changePages(currentPage+1)}>
-//                 <i className="fas fa-step-forward"></i>
-//             </button>
-//             <button id="btnEnd" className="paginationBtn" onClick={() => changePages(Number(totalPages))}>
-//                 <i className="fas fa-forward"></i>
-//             </button>
-//         </div>
-//         )
-// }
-
-
-// export {Paginator}
-
-// const useStyles = makeStyles(theme =>
-//   ({
-//     root: {
-//         position: 'fixed',
-//         backgroundColor: theme.palette.red,
-//         bottom: 0,
-//         zIndex: 200,
-//         padding: '10px 80px'
-//     },
-//     container: {
-
-//     }
-//   }),
-// );
-
-// type Props = {
-//     totalPages: string;
-// } 
 
 const BasicPagination: FC = () => {
 
@@ -64,7 +14,6 @@ const BasicPagination: FC = () => {
          setPage(value);
          window.scroll(0,0);
     }
-    //const classes = useStyles();
 
     return (
       <Stack spacing={2}>
