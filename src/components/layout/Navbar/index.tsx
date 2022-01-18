4ea4fe02dbae640b21f0f920a9d41b805428164d
MarianaDeleau <marianadeleau@gmail.com>
2022-01-18 09:20:15 -0300

    fix mui bar routing

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -7,13 +7,14 @@ import PersonPinIcon from '@mui/icons-material/PersonPin';
 import LockIcon from '@mui/icons-material/Lock';
 import MovieIcon from '@mui/icons-material/Movie';
 import { ExitToAppRounded } from '@mui/icons-material';
-import { NavLink} from 'react-router-dom';
+import { NavLink, useLocation} from 'react-router-dom';
 import { useUsers } from '../../../hooks';
 
 
 const NavbarApp = () => {
   const [value, setValue] = React.useState(0);
-
+  const location = useLocation()
+  const routes = ['/', '/movies', '/series', '/users', '/admin', '/' ]
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -21,13 +22,13 @@ const NavbarApp = () => {
 const { logout } = useUsers()
 
   return (
-    <Tabs  value={false} onChange={handleChange} aria-label="icon label tabs example">      
-      <Tab icon={<HomeIcon />} label="HOME" component={NavLink} to={'/'} />
-      <Tab icon={<MovieIcon />} label="MOVIES" component={NavLink} to={'/movies'} />
-      <Tab icon={<TvRoundedIcon />} label="SERIES" component={NavLink} to={'/series'}/>
-      <Tab icon={<PersonPinIcon />} label="USERS" component={NavLink} to={'/users'}/>
-      <Tab icon={<LockIcon />} label="ADMIN" component={NavLink} to={'/admin'}/>
-      <Tab icon={<ExitToAppRounded />} label="EXIT" component={NavLink} to={'/'} onClick={logout}/>
+    <Tabs  value={location.pathname} onChange={handleChange} aria-label="icon label tabs example">      
+      <Tab icon={<HomeIcon />} label="HOME" component={NavLink} to={'/'} value={routes[0]}/>
+      <Tab icon={<MovieIcon />} label="MOVIES" component={NavLink} to={'/movies'} value={routes[1]}/>
+      <Tab icon={<TvRoundedIcon />} label="SERIES" component={NavLink} to={'/series'} value={routes[2]}/>
+      <Tab icon={<PersonPinIcon />} label="USERS" component={NavLink} to={'/users'} value={routes[3]}/>
+      <Tab icon={<LockIcon />} label="ADMIN" component={NavLink} to={'/admin'} value={routes[4]}/>
+      <Tab icon={<ExitToAppRounded />} label="EXIT" component={NavLink} to={'/'} onClick={logout} value={false}/>
     </Tabs>
   );
 };
