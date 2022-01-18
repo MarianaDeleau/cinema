96fefbab1b79ef5735debd938d3b500879e0cdd2
MarianaDeleau <marianadeleau@gmail.com>
2022-01-18 19:43:10 -0300

    fix navbar conditional rendering

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -9,12 +9,17 @@ import MovieIcon from '@mui/icons-material/Movie';
 import { ExitToAppRounded } from '@mui/icons-material';
 import { NavLink, useLocation} from 'react-router-dom';
 import { useUsers } from '../../../hooks';
-
+import {PersonOutline, PersonAdd}  from '@mui/icons-material';
 
 const NavbarApp = () => {
   const [value, setValue] = React.useState(0);
   const location = useLocation()
-  const routes = ['/', '/movies', '/series', '/users', '/admin', '/' ]
+
+  const { userLogged } = useUsers()
+
+  const routes = ['/signup', '/login', '/', '/movies', '/series', '/users', '/admin', '/' ]
+
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -24,11 +29,13 @@ const { logout } = useUsers()
   return (
   
     <Tabs  value={location.pathname} onChange={handleChange} aria-label="icon label tabs example">      
-      <Tab icon={<HomeIcon />} label="HOME" component={NavLink} to={'/'} value={routes[0]}/>
-      <Tab icon={<MovieIcon />} label="MOVIES" component={NavLink} to={'/movies'} value={routes[1]}/>
-      <Tab icon={<TvRoundedIcon />} label="SERIES" component={NavLink} to={'/series'} value={routes[2]}/>
-      <Tab icon={<PersonPinIcon />} label="USERS" component={NavLink} to={'/users'} value={routes[3]}/>
-      <Tab icon={<LockIcon />} label="ADMIN" component={NavLink} to={'/admin'} value={routes[4]}/>
+      {!userLogged && <Tab icon={<PersonAdd />} label="SIGNUP" component={NavLink} to={'/signup'} value={routes[0]}/>}
+      {!userLogged &&  <Tab icon={<PersonOutline />} label="LOGIN" component={NavLink} to={'/login'} value={routes[1]}/> }
+      <Tab icon={<HomeIcon />} label="HOME" component={NavLink} to={'/'} value={routes[2]}/>
+      <Tab icon={<MovieIcon />} label="MOVIES" component={NavLink} to={'/movies'} value={routes[3]}/>
+      <Tab icon={<TvRoundedIcon />} label="SERIES" component={NavLink} to={'/series'} value={routes[4]}/>
+      <Tab icon={<PersonPinIcon />} label="USERS" component={NavLink} to={'/users'} value={routes[5]}/>
+      <Tab icon={<LockIcon />} label="ADMIN" component={NavLink} to={'/admin'} value={routes[6]}/>
       <Tab icon={<ExitToAppRounded />} label="EXIT" component={NavLink} to={'/'} onClick={logout} value={false}/>
     </Tabs>
 
