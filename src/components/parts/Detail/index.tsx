28e086a657e27333d1091f97a25bef701f3e1eca
MarianaDeleau <marianadeleau@gmail.com>
2022-01-22 17:54:47 -0300

    fix videos layout

--- src/components/parts/Detail/index.tsx
+++ src/components/parts/Detail/index.tsx
@@ -36,12 +36,12 @@ movieApi.getTrailers(movieDetail?.id!)
   
   console.log(trailers)
     return (
-          <div className="container d-flex flex-wrap justify-content-center ">        
-          <Card sx={{ width: 'auto', margin: 2 , height: 800,  display: 'flex', borderRadius: 15, padding: 4}} className="card_detail">
+          <div className="d-flex flex-wrap justify-content-center ">        
+          <Card sx={{ width: '100%', margin: 2 , height: 'auto',  display: 'flex', borderRadius: 15, padding: 3}} className="card_detail">
               <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                 <CardActionArea>
-                  <CardContent sx={{ height: 300 }}>
-                    <Typography  component="div" sx={{ color: 'gray', fontWeight: 600, fontSize: 30, textAlign: 'center', lineHeight: 1.2, marginTop: 10 }}>
+                  <CardContent sx={{ height: 'auto' }}>
+                    <Typography  component="div" sx={{ color: 'gray', fontWeight: 600, fontSize: 30, textAlign: 'center', lineHeight: 1.2, marginTop: 5 }}>
                     <p>{movieDetail?.title || movieDetail?.name}</p>
                     </Typography>
                     <Typography variant="body2" sx={{ color: 'gray', fontWeight: 500, fontSize: 15, textAlign: 'center', lineHeight: 1.2   }}>
@@ -53,14 +53,15 @@ movieApi.getTrailers(movieDetail?.id!)
                     <Typography variant="body2" sx={{ color: 'gray', fontWeight: 400, fontSize: 15, textAlign: 'center', lineHeight: 1.2   }}>
                     <p>Language: {movieDetail?.original_language}</p>
                     </Typography>
-                    <Box sx={{ '& > legend': { mt: 2 }, color: "gray", textAlign: 'center', display: 'flex', flexWrap: 'nowrap' } } >      
-                          <Rating name="read-only" value={movieDetail?.vote_average &&movieDetail?.vote_average/2} precision={0.5} max={5} readOnly />                  
+                  </CardContent>
+                    <Box sx={{ '& > legend': { mt: 2 }, color: "gray", textAlign: 'center' } } >      
+                          <Rating name="read-only" value={movieDetail?.vote_average &&movieDetail?.vote_average/2} precision={0.5} max={5} sx={{mb: 5}} readOnly />                  
                     </Box>    
-                    <Box sx={{ '& > legend': { mt: 2 }, color: "gray", textAlign: 'center' }} >      
+                    <Box sx={{ '& > legend': { mt: 2 }, color: 'gray', textAlign: 'center', display: 'flex', flexWrap: 'wrap' }} >      
                     {trailers?.map((video) => (
                           <div  className={`col-md-${ trailers.length === 1 ? 12 : 6 } mb-3`} >
                             <iframe
-                              width="100%"
+                              width="90%"
                               height={trailers.length === 1 ? 320 : 215}
                               src={`https://www.youtube.com/embed/${video.key}`}
                               title="YouTube video player"
@@ -71,7 +72,6 @@ movieApi.getTrailers(movieDetail?.id!)
                          </div>
                         ))}        
                     </Box> 
-                  </CardContent>
                 </CardActionArea>
               </Box>
             <CardMedia component="img"  /*height="300" width="50"*/ sx={{width: 400, height: 600, borderRadius: 10}} image={`http://image.tmdb.org/t/p/w500${movieDetail?.poster_path}`} alt={movieDetail?.title || movieDetail?.name}/>
