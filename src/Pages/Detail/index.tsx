243ad86aaaeb76685eff2e0595030ce9e6b324d0
MarianaDeleau <marianadeleau@gmail.com>
2022-01-25 11:04:52 -0300

    add goBack button and onclick

--- src/Pages/Detail/index.tsx
+++ src/Pages/Detail/index.tsx
@@ -3,10 +3,11 @@ import { Layout } from '../../components'
 import { WithAuth } from '../../hoc';
 import { DetailCard } from '../../components/parts/Detail'
 import { UserCardMovie } from '../../components/parts/UserCardMovie';
-
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeftCircle } from "react-bootstrap-icons";
 const DetailPage: FC = () => {
   
-  
+  const navigate = useNavigate()
   
     return (
       <>
@@ -14,8 +15,9 @@ const DetailPage: FC = () => {
         <div className="container">
           <DetailCard/>
         </div>
-        <div className="container d-flex  justify-content-start me-5 mt-5">
-          <h2>Also...</h2>
+        <div className="container d-flex  me-5 mt-5 flex-column">
+          <ArrowLeftCircle fontSize={40} className='mt-5 mb-5 d-flex align-self-center' color='black' onClick={() => navigate(-1)}/>
+          <h2 className='d-flex text-align-start'>Also...</h2>
         </div>
         <div className="container d-flex flex-wrap justify-content-center">
           <UserCardMovie media_type='movie'/>        
