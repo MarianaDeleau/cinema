575fbebe1ebc38131919b32fe55bf94628162202
MarianaDeleau <marianadeleau@gmail.com>
2022-02-19 10:07:35 -0300

    fix admin buttons in home

--- src/components/parts/CardMovie/index.tsx
+++ src/components/parts/CardMovie/index.tsx
@@ -1,53 +1,105 @@
-import { FC } from "react"
+import { FC } from "react";
 import { useItems } from "../../../hooks/useItems";
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
 import { Card } from "@mui/material";
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import { movieApi } from '../../../api';
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import { movieApi } from "../../../api";
 
 const CardMovie: FC = () => {
-    
-    const { items, IsMovieInDB, itemsDB, deleteMoviesFromDB, addMovieToDB } = useItems();  
+  const { items, IsMovieInDB, deleteMoviesFromDB, addMovieToDB } = useItems();
 
-
+  return (
+    <div className="container d-flex flex-wrap justify-content-center ">
+      {items?.results.map((item) => {
         return (
-          <div className="container d-flex flex-wrap justify-content-center ">  
-            {items?.results.map ((item)=>  { return (
-            <Card sx={{ width: 200, margin: 2 }} className="card_movie">
-              <CardActionArea>
-                <CardMedia component="img"  height="300" width="150" image={`http://image.tmdb.org/t/p/w500${item.poster_path}`}
-                  alt={item.title || item.name}
-                />
-                <CardContent sx={{ height: 200 }}>
-                  <Typography  component="div" sx={{ color: 'gray', fontWeight: 500, fontSize: 18, textAlign: 'center', lineHeight: 1.2 }}>
+          <Card sx={{ width: 200, margin: 2 }} className="card_movie">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="300"
+                width="150"
+                image={`http://image.tmdb.org/t/p/w500${item.poster_path}`}
+                alt={item.title || item.name}
+              />
+              <CardContent sx={{ height: 200 }}>
+                <Typography
+                  component="div"
+                  sx={{
+                    color: "gray",
+                    fontWeight: 500,
+                    fontSize: 18,
+                    textAlign: "center",
+                    lineHeight: 1.2,
+                  }}
+                >
                   <p>{item.title || item.name}</p>
-                  </Typography>
-                  <Typography variant="body2" sx={{ color: 'gray', fontWeight: 400, fontSize: 15, textAlign: 'center', lineHeight: 1.2   }}>
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    color: "gray",
+                    fontWeight: 400,
+                    fontSize: 15,
+                    textAlign: "center",
+                    lineHeight: 1.2,
+                  }}
+                >
                   <p>{item.vote_average}</p>
-                  </Typography>
+                </Typography>
                 <Box
-                      sx={{ '& > legend': { mt: 2 }, color: "gray", textAlign: 'center' }} >      
-                      <Rating name="read-only" value={item.vote_average && item.vote_average/2} precision={0.5} max={5} readOnly />                  
+                  sx={{
+                    "& > legend": { mt: 2 },
+                    color: "gray",
+                    textAlign: "center",
+                  }}
+                >
+                  <Rating
+                    name="read-only"
+                    value={item.vote_average && item.vote_average / 2}
+                    precision={0.5}
+                    max={5}
+                    readOnly
+                  />
                 </Box>
-                </CardContent>
-              </CardActionArea>
-              {!IsMovieInDB(item.id) && <CardActions sx={{ justifyContent: 'center' }}>
-              <Button size="small" sx={{ backgroundColor: 'red', width: 120 }} type="submit" onClick={()=> {addMovieToDB(item)} }>AGREGAR</Button> 
-              </CardActions>}
-              {IsMovieInDB(item.id) &&<CardActions sx={{ justifyContent: 'center' }}>
-              <Button size="small" sx={{ backgroundColor: 'black', width: 120 }} type="submit" onClick={()=> {deleteMoviesFromDB(item.id)} }>REMOVER</Button>
-              </CardActions>}
-            </Card>
-            )})}
-          </div>
-          );
+              </CardContent>
+            </CardActionArea>
+            {!IsMovieInDB(item.id) && (
+              <CardActions sx={{ justifyContent: "center" }}>
+                <Button
+                  size="small"
+                  sx={{ backgroundColor: "red", width: 120 }}
+                  type="submit"
+                  onClick={() => {
+                    addMovieToDB(item);
+                  }}
+                >
+                  AGREGAR
+                </Button>
+              </CardActions>
+            )}
+            {IsMovieInDB(item.id) && (
+              <CardActions sx={{ justifyContent: "center" }}>
+                <Button
+                  size="small"
+                  sx={{ backgroundColor: "black", width: 120 }}
+                  type="submit"
+                  onClick={() => {
+                    deleteMoviesFromDB(item.id);
+                  }}
+                >
+                  REMOVER
+                </Button>
+              </CardActions>
+            )}
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
 
-          
-      }
-   
-
-export { CardMovie }
+export { CardMovie };
